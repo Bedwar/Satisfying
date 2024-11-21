44a271c75088ec1d80e568e9992c207bfb4803c0
BDora <bwaricoda@alunos.utfpr.edu.br>
2024-11-21 20:53:02 -0300

    Ajuste de Cores

--- src/screens/Relatorio.js
+++ src/screens/Relatorio.js
@@ -8,11 +8,11 @@ import relatorioStyles from '../styles/screens/relatorioStyles' ;
 
 const Relatorio = (props) => {
   const data = [
-    { name: 'Excelente', population: 25, color: 'yellow', legendFontColor: 'white', legendFontSize: 15 },
-    { name: 'Bom', population: 20, color: 'lightskyblue', legendFontColor: 'white', legendFontSize: 15 },
-    { name: 'Neutro', population: 15, color: 'green', legendFontColor: 'white', legendFontSize: 15 },
-    { name: 'Ruim', population: 10, color: 'tomato', legendFontColor: 'white', legendFontSize: 15 },
-    { name: 'Péssimo', population: 5, color: 'darkturquoise', legendFontColor: 'white', legendFontSize: 15 },
+    { name: 'Excelente', population: 60, color: 'limegreen', legendFontColor: 'white', legendFontSize: 15 },
+    { name: 'Bom', population: 20, color: 'blue', legendFontColor: 'white', legendFontSize: 15 },
+    { name: 'Neutro', population: 10, color: 'gold', legendFontColor: 'white', legendFontSize: 15 },
+    { name: 'Ruim', population: 5, color: 'darkorange', legendFontColor: 'white', legendFontSize: 15 },
+    { name: 'Péssimo', population: 5, color: 'crimson', legendFontColor: 'white', legendFontSize: 15 },
   ];
 
   return (
